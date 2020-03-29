301d51b639ffcbd898bd93b8963e7468f79e658c
HallexCosta <hallex.costa@hotmail.com>
2020-03-29 20:35:20 -0300

    Corrindo nome da variavel validatorIncidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,7 +7,7 @@ const SessionController = require('./controllers/SessionController')
 
 const validatorOngs = require('./validators/ValidatorOngs')
 const validatorSessions = require('./validators/ValidatorSessions')
-const validatorIncidens = require('./validators/ValidatorIncidents')
+const validatorIncidents = require('./validators/ValidatorIncidents')
 const validatorProfile = require('./validators/ValidatorProfile')
 
 const routes = express.Router()
@@ -17,8 +17,8 @@ routes.post('/ongs', validatorOngs.create(), OngController.create)
 
 routes.post('/sessions', validatorSessions.session(), SessionController.create)
 
-routes.get('/incidents', validatorIncidens.list(), IncidentController.index)
-routes.post('/incidents', validatorIncidens.create(), IncidentController.create)
+routes.get('/incidents', validatorIncidents.list(), IncidentController.index)
+routes.post('/incidents', validatorIncidents.create(), IncidentController.create)
 routes.delete('/incidents/:id', validatorIncidents.delete(), IncidentController.delete)
 
 routes.get('/profile', validatorProfile.list(), ProfileController.index)
